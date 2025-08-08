ab0ca22291d7300df843ea7a0711ec1a07f2aa93
salauddin85 <ahmedsalauddin677785@gmail.com>
2025-08-09 00:35:44 +0600

    learned array and object distructuring

---
+++ module1/dist/1.8.js
@@ -0,0 +1,24 @@
+"use strict";
+{
+    // object destructuring
+    const user = {
+        id: 434,
+        names: {
+            rahim: {
+                firstName: 'Rahim',
+                lastName: 'Rahman'
+            },
+            karim: {
+                firstName: 'Karim',
+                lastName: 'Karim'
+            },
+            age: 34,
+            contactNo: '095349'
+        }
+    };
+    const { id, names: { rahim: { firstName, lastName } } } = user;
+    console.log(firstName, lastName);
+    // array destructuring
+    const [, , third, ...others] = [1, 2, 3, 4, 5, 6, 7];
+    console.log(third, others);
+}
